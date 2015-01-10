ee23816608206a66d053321a2050fa12996ca1ba
BramBrouwer <b.brouwer@student.avans.nl>
2015-01-10 15:36:50 +0100

    ProductWindow
    Added and implemented

---
+++ WebWinkel2.0/WebWinkel2.0/ViewModel/ProductListViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WebWinkel2._0.Repositories;
+
+namespace WebWinkel2._0.ViewModel
+{
+    public class ProductListViewModel : INotifyPropertyChanged
+    {
+
+          //for the purpose of navigation
+     // private int _index;
+
+      //our repository for testing purposes 
+      IProductRepository productrepository;
+
+      //lijst van songview models 
+      public ObservableCollection<ProductViewModel> Producten { get; set; }
+
+      //selected object
+      private ProductViewModel _selectedProduct;
+
+      //reference property for product
+      public ProductViewModel SelectedProduct
+      {
+          get
+          {
+              return _selectedProduct;
+          }
+          set
+          {
+              _selectedProduct = value;
+             //!!!!!!!!!!!!!!!!!!!!!!!!!!
+              // RaisePropertyChanged();
+          }
+      }
+
+
+      //class constructor
+      public ProductListViewModel()
+      {
+          productrepository = new Productrepository();
+          var productLijst = productrepository.ToList().Select(a => new ProductViewModel(a));
+          Producten = new ObservableCollection<ProductViewModel>(productLijst);
+      }
+  
+
+
+
+
+     
+
+
+      public event PropertyChangedEventHandler PropertyChanged;
+      protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+      {
+          if (PropertyChanged != null)
+          {
+              PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+          }
+      }
+    
+  }
+
+    }
+
--- WebWinkel2.0/WebWinkel2.0/ViewModel/WindowsViewModel.cs
+++ WebWinkel2.0/WebWinkel2.0/ViewModel/WindowsViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using WebWinkel2._0.Views;
 
 namespace WebWinkel2._0.ViewModel
 {//this class is added for the purpose of navigation trough all windows. 
@@ -13,25 +14,28 @@ namespace WebWinkel2._0.ViewModel
     {
         //private references to all windows
         private AfdelingWindow _AfdelingWindow;
+        private ProductWindow _ProductWindow;
        
       //add different icommands per window
         public ICommand ShowAfdelingWindowCommand { get; set; }
+        public ICommand ShowProductWindowCommand { get; set; }
 
         public WindowsViewModel()
         {
             _AfdelingWindow = new AfdelingWindow();
-           
+            _ProductWindow = new ProductWindow();
 
             ShowAfdelingWindowCommand = new RelayCommand(showAfdelingWindow, canShowAfdelingWindow);
-           
+            ShowProductWindowCommand = new RelayCommand(showProductWindow, canShowProductWindow);
         }
 
 
-        private bool canShowAfdelingWindow()
-        {
-            return _AfdelingWindow.IsVisible == false;
-        }
 
+
+
+
+        //methods that get called when the command is called
+        #region afdeling
         private void showAfdelingWindow()
         {
             try { _AfdelingWindow.Show(); }
@@ -46,8 +50,33 @@ namespace WebWinkel2._0.ViewModel
            
         }
 
+        private bool canShowAfdelingWindow()
+        {
+            return _AfdelingWindow.IsVisible == false;
+        }
+        #endregion
 
-      
+        #region product
+        private void showProductWindow()
+        {
+            try {_ProductWindow.Show(); }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                _ProductWindow = new ProductWindow();
+               _ProductWindow.InitializeComponent();
+                _ProductWindow.Show();
+            }
+
+        }
+
+        private bool canShowProductWindow()
+        {
+            return _ProductWindow.IsVisible == false;
+        }
+        #endregion
+
 
 
 
