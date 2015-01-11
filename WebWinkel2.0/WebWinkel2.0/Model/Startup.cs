dd3014af1034071891c45e76ba8cc823e6fa685b
Spirit-Balanceholder <martinkleinpaste@hotmail.com>
2015-01-11 14:15:12 +0100

    Revert "Revert "bla""
    This reverts commit c89ed51de9ee6d984150b43aa20ffe9bbc468ca1.

---
+++ WebWinkel2.0/WebWinkel2.0/Model/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebWinkel2._0.Model
+{
+    public static class Startup
+    {
+        public static void AppLaunch()
+        {
+            TestWaarden ts = new TestWaarden();
+            ts.EmptyFB();
+            ts.InsertIntoDB();
+        }
+    }
+}
