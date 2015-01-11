3736d6ac684f20c54de35e03f68cda572a408a31
Spirit-Balanceholder <martinkleinpaste@hotmail.com>
2015-01-11 15:37:11 +0100

    yolo

---
+++ WebWinkel2.0/WebWinkel2.0/Model/Recept_Has_Eindproduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebWinkel2._0.Model
+{
+    public class Recept_Has_Eindproduct
+    {
+        [Required]
+        [Key]
+        [ForeignKey("Eindproduct")]
+        [Column(Order = 1)] 
+        public int? EindproductID { get; set; }
+        public virtual Eindproduct Eindproduct { get; set; }
+
+        [Required]
+        [Key]
+        [ForeignKey("Recept")]
+        [Column(Order = 2)] 
+        public int? ReceptID { get; set; }
+
+        public virtual Recept Recept { get; set; }
+    }
+}
